d19d95cf59a34f126e891ede1ad0067ce0f67efa
vytska69 <vytautas.duchas@gmail.com>
2021-12-30 10:49:57 +0200

    Update lineage_pine.mk

--- lineage_pine.mk
+++ lineage_pine.mk
@@ -12,6 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit some common PixelExperience stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 720
+
 # Inherit from pine device
 $(call inherit-product, device/xiaomi/pine/device.mk)
 
