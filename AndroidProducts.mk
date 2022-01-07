4069e433b5a600e7876057473b62afd0a20f7af7
vytska69 <vytautas.duchas@gmail.com>
2022-01-07 20:55:57 +0200

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_pine.mk
+    $(LOCAL_DIR)/superior_pine.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_pine-user \
-    aosp_pine-userdebug \
-    aosp_pine-eng
+    superior_pine-user \
+    superior_pine-userdebug \
+    superior_pine-eng
