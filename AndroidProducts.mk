fcfdab9cbf45054cb45aa13444a1ac41d6af189e
vytska69 <vytautas.duchas@gmail.com>
2022-01-11 15:32:28 +0200

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_pine.mk
+    $(LOCAL_DIR)/derp_pine.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_pine-user \
-    aosp_pine-userdebug \
-    aosp_pine-eng
+    derp_pine-user \
+    derp_pine-userdebug \
+    derp_pine-eng
