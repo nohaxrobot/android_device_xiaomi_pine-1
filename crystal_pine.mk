050602e889c1451319be757bf73d03f032a5ebca
vytska69 <vytautas.duchas@gmail.com>
2022-01-18 18:29:24 +0200

    Update crystal_pine.mk

--- crystal_pine.mk
+++ crystal_pine.mk
@@ -28,8 +28,3 @@ TARGET_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="pine-user 10 QKQ1.191014.001 V12.5.1.0.QCMCNXM release-keys"
-
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.211205.016.A1/7957957:user/release-keys"
